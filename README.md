ece1b377dfb363e077eb952fc3c6aa4907bf961f
flankerad <flanker.ad@gmail.com>
2013-08-26 20:11:54 +0530

    Added files

---
+++ README.md
@@ -0,0 +1,16 @@
+CSS Social Icons Sprite
+
+Simple Ready to use Social Icon sprite.
+
+Icons Reso available
+
+32x32
+64x64
+128x128
+
+Usage
+
+Copy the HTML code whene you want to use icons in your HTML file. Copy the corresponding CSS code either paste it as shown in file or at bottom of your seprate CSS file.
+
+If using all 3 versions simply rename to files and code to use simultaneously.
+
---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name='description' content='Free Css Sprite Social Icon pack of 5 icons if you like it or want more like this '>
+</head>
+
+<header>
+  
+</header>
+
+<body> 
+
+ <div id="main">
+
+ <!-- *********    If you want 128x128 resolution icons use the code below   *********-->
+ 
+ <style>
+		#slide{position:absolute;top:40%;left:20%;color:#ababab;}
+		#slide img{width:128px;height:128px;border:1px solid #232b2b2b}
+		
+		#fb{background-image:url(img/128x128.png);background-position:0% 0%;}
+		#fb:hover{background-position:0% 98%;}
+
+		#yu{background-image:url(img/128x128.png);background-position:25% 0%;}
+		#yu:hover{background-position:25% 98%;}
+
+		#rss{background-image:url(img/128x128.png);background-position:50% 0%;}
+		#rss:hover{background-position:50% 98%;}
+
+		#t{background-image:url(img/128x128.png);background-position:74.8% 0%;}
+		#t:hover{background-position:74.9% 102%;}
+
+		#g{background-image:url(img/128x128.png);background-position:100% 0%;}
+		#g:hover{background-position:100% 98%;}
+ </style>
+
+<div id="slide">
+<span id='topicons'>
+	<img id='fb' src="img/transparent128x128.png" />
+	<img id='yu'src="img/transparent128x128.png" />
+	<img id='rss'src="img/transparent128x128.png" />
+	<img id='t'src="img/transparent128x128.png" />
+	<img id='g'src="img/transparent128x128.png" />
+</span>
+</div>	
+
+
+ <!-- *********    If you want 64x64 resolution icons use the code below     ********* -->
+ 
+ <style>
+		#slide{position:absolute;top:40%;left:30%;color:#ababab;}
+		#slide img{width:64px;height:64px;border:1px solid #232b2b2b}
+		
+		#fb{background-image:url(img/64x64.png);background-position:0% 0%;}
+		#fb:hover{background-position:0% 98%;}
+
+		#yu{background-image:url(img/64x64.png);background-position:25% 0%;}
+		#yu:hover{background-position:25% 98%;}
+
+		#rss{background-image:url(img/64x64.png);background-position:50% 0%;}
+		#rss:hover{background-position:50% 98%;}
+
+		#t{background-image:url(img/64x64.png);background-position:74.8% 0%;}
+		#t:hover{background-position:74.9% 102%;}
+
+		#g{background-image:url(img/64x64.png);background-position:100% 0%;}
+		#g:hover{background-position:100% 98%;}
+ </style>
+
+<div id="slide">
+<span id='topicons'>
+	<img id='fb' src="img/transparent64x64.png" />
+	<img id='yu'src="img/transparent64x64.png" />
+	<img id='rss'src="img/transparent64x64.png" />
+	<img id='t'src="img/transparent64x64.png" />
+	<img id='g'src="img/transparent64x64.png" />
+</span>
+</div>	
+
+ <!-- *********   If you want 32x32 resolution icons use the code below    ********* -->
+ 
+ <style>
+     	#slide{position:absolute;top:40%;left:30%;color:#ababab;}
+		#slide img{width:32px;height:32px;border:1px solid #232b2b2b}
+		
+		#fb{background-image:url(img/32x32.png);background-position:0% 0%;}
+		#fb:hover{background-position:0% 98%;}
+
+		#yu{background-image:url(img/32x32.png);background-position:25.5% 0%;}
+		#yu:hover{background-position:25% 98%;}
+
+		#rss{background-image:url(img/32x32.png);background-position:50% 0%;}
+		#rss:hover{background-position:50% 98%;}
+
+		#t{background-image:url(img/32x32.png);background-position:74.8% 0%;}
+		#t:hover{background-position:74.9% 98%;}
+
+		#g{background-image:url(img/32x32.png);background-position:100% 0%;}
+		#g:hover{background-position:100% 98%;}
+ </style>
+
+ <div id="slide">
+<span id='topicons'>
+	<img id='fb' src="img/transparent32x32.png" />
+	<img id='yu'src="img/transparent32x32.png" />
+	<img id='rss'src="img/transparent32x32.png" />
+	<img id='t'src="img/transparent32x32.png" />
+	<img id='g'src="img/transparent32x32.png" />
+</span>
+</div>	
+
+</div>
+</body>
