1fb36065ee153a93644fe8589a8b13957a2b469c
flankerad <flanker.ad@gmail.com>
2013-09-06 16:57:36 +0530

    made presentable

--- index.php
+++ index.php
@@ -3,47 +3,47 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <meta name='description' content='Free Css Sprite Social Icon pack of 5 icons if you like it or want more like this '>
+  <meta name='description' content='Free Css Sprite Social Icon pack of 5 icons'>
 </head>
 
-<body> 
+<body style="background:#171624"> 
 
  <div id="main">
 
- <!-- *********    If you want 128x128 resolution icons use the code below   *********-->
+ <!-- *********   If you want 32x32 resolution icons uncomment use the code below    ********* >
  
  <style>
-		#slide{position:absolute;top:40%;left:20%;color:#ababab;}
-		#slide img{width:128px;height:128px;border:1px solid #232b2b2b}
+     	#slide{position:absolute;top:40%;left:30%;color:#ababab;}
+		#slide img{width:32px;height:32px;border:1px solid #232b2b2b}
 		
-		#fb{background-image:url(img/128x128.png);background-position:0% 0%;}
+		#fb{background-image:url(img/32x32.png);background-position:0% 0%;}
 		#fb:hover{background-position:0% 98%;}
 
-		#yu{background-image:url(img/128x128.png);background-position:25% 0%;}
+		#yu{background-image:url(img/32x32.png);background-position:25.5% 0%;}
 		#yu:hover{background-position:25% 98%;}
 
-		#rss{background-image:url(img/128x128.png);background-position:50% 0%;}
+		#rss{background-image:url(img/32x32.png);background-position:50% 0%;}
 		#rss:hover{background-position:50% 98%;}
 
-		#t{background-image:url(img/128x128.png);background-position:74.8% 0%;}
-		#t:hover{background-position:74.9% 102%;}
+		#t{background-image:url(img/32x32.png);background-position:74.8% 0%;}
+		#t:hover{background-position:74.9% 98%;}
 
-		#g{background-image:url(img/128x128.png);background-position:100% 0%;}
+		#g{background-image:url(img/32x32.png);background-position:100% 0%;}
 		#g:hover{background-position:100% 98%;}
  </style>
 
-<div id="slide">
+
+ <div id="slide">
 <span id='topicons'>
-	<img id='fb' src="img/transparent128x128.png" />
-	<img id='yu'src="img/transparent128x128.png" />
-	<img id='rss'src="img/transparent128x128.png" />
-	<img id='t'src="img/transparent128x128.png" />
-	<img id='g'src="img/transparent128x128.png" />
+	<img id='fb' src="img/transparent32x32.png" />
+	<img id='yu'src="img/transparent32x32.png" />
+	<img id='rss'src="img/transparent32x32.png" />
+	<img id='t'src="img/transparent32x32.png" />
+	<img id='g'src="img/transparent32x32.png" />
 </span>
-</div>	
+</div>
 
-
- <!-- *********    If you want 64x64 resolution icons use the code below     ********* -->
+ <!-- *********    If you want 64x64 resolution icons use the code below     ********* >
  
  <style>
 		#slide{position:absolute;top:40%;left:30%;color:#ababab;}
@@ -75,35 +75,36 @@
 </span>
 </div>	
 
- <!-- *********   If you want 32x32 resolution icons use the code below    ********* -->
+
+ <!-- *********    If you want 128x128 resolution icons use the code below   *********-->
  
  <style>
-     	#slide{position:absolute;top:40%;left:30%;color:#ababab;}
-		#slide img{width:32px;height:32px;border:1px solid #232b2b2b}
+		#slide{position:absolute;top:40%;left:20%;color:#ababab;}
+		#slide img{width:128px;height:128px;border:1px solid #232b2b2b}
 		
-		#fb{background-image:url(img/32x32.png);background-position:0% 0%;}
+		#fb{background-image:url(img/128x128.png);background-position:0% 0%;}
 		#fb:hover{background-position:0% 98%;}
 
-		#yu{background-image:url(img/32x32.png);background-position:25.5% 0%;}
+		#yu{background-image:url(img/128x128.png);background-position:25% 0%;}
 		#yu:hover{background-position:25% 98%;}
 
-		#rss{background-image:url(img/32x32.png);background-position:50% 0%;}
+		#rss{background-image:url(img/128x128.png);background-position:50% 0%;}
 		#rss:hover{background-position:50% 98%;}
 
-		#t{background-image:url(img/32x32.png);background-position:74.8% 0%;}
-		#t:hover{background-position:74.9% 98%;}
+		#t{background-image:url(img/128x128.png);background-position:74.8% 0%;}
+		#t:hover{background-position:74.9% 102%;}
 
-		#g{background-image:url(img/32x32.png);background-position:100% 0%;}
+		#g{background-image:url(img/128x128.png);background-position:100% 0%;}
 		#g:hover{background-position:100% 98%;}
  </style>
 
- <div id="slide">
+<div id="slide">
 <span id='topicons'>
-	<img id='fb' src="img/transparent32x32.png" />
-	<img id='yu'src="img/transparent32x32.png" />
-	<img id='rss'src="img/transparent32x32.png" />
-	<img id='t'src="img/transparent32x32.png" />
-	<img id='g'src="img/transparent32x32.png" />
+	<img id='fb' src="img/transparent128x128.png" />
+	<img id='yu'src="img/transparent128x128.png" />
+	<img id='rss'src="img/transparent128x128.png" />
+	<img id='t'src="img/transparent128x128.png" />
+	<img id='g'src="img/transparent128x128.png" />
 </span>
 </div>	
 
